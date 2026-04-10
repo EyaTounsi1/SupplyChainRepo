936c6d80082084877db2a3f683ae5f80c2e2896e
EyaTounsi1 <154350414+EyaTounsi1@users.noreply.github.com>
2026-04-10 15:35:14 +0200

    Fix navigation links for deviation report pages and update sidebar routes

--- Models/AutomationDbContext.cs
+++ Models/AutomationDbContext.cs
@@ -1,58 +1,64 @@
+
 using Microsoft.EntityFrameworkCore;
 
-namespace PartTracker.Models;
-
-public class AutomationDbContext : DbContext
+namespace PartTracker.Models
 {
-    public DbSet<ActivityForm> Activity { get; set; }
-    public DbSet<SplunkEntry> Splunk { get; set; }
-    public DbSet<HelperEntry> Helper { get; set; }
-    public DbSet<SpeedUpRequest> SpeedUpRequests { get; set; }
-    public DbSet<Premium2025> Premiums2025 { get; set; }
-    public DbSet<Premium2024> Premiums2024 { get; set; }
-    public DbSet<PartPrice> PartPrices { get; set; }
-    public DbSet<SafetyStockFormEntry> SafetyStockForms { get; set; }
-    public DbSet<TestCrudSafety> TestCrudSafeties { get; set; }
-    public DbSet<AftermarketCollectionModel> AftermarketCollections { get; set; }
-    public DbSet<SpeedUpModel> SpeedUps { get; set; }
-    public DbSet<PremiumBookingUpdate> PremiumBookingUpdates { get; set; }
-
-    public AutomationDbContext(DbContextOptions<AutomationDbContext> options) : base(options) { }
-
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    public class AutomationDbContext : DbContext
     {
-        base.OnModelCreating(modelBuilder);
-        
-        modelBuilder.Entity<ActivityForm>().ToTable("activity");
-        modelBuilder.Entity<SplunkEntry>()
-            .ToTable("splunk")
-            .HasKey(s => s.LoadNumber);
-        modelBuilder.Entity<HelperEntry>()
-            .ToTable("helper")
-            .HasKey(h => h.Id);
-        modelBuilder.Entity<SpeedUpRequest>()
-            .ToTable("ibl_emea_speedup_requests")
-            .HasKey(s => s.Id);
-        modelBuilder.Entity<Premium2025>()
-            .ToTable("premiums2025")
-            .HasKey(p => p.Id);
-        modelBuilder.Entity<Premium2024>()
-            .ToTable("premiums2024")
-            .HasKey(p => p.Id);
-        modelBuilder.Entity<PartPrice>()
-            .ToTable("Part_Price");
-        modelBuilder.Entity<SafetyStockFormEntry>()
-            .ToTable("safetystockform")
-            .HasKey(s => s.Id);
-        modelBuilder.Entity<TestCrudSafety>()
-            .ToTable("TestCrudSafety")
-            .HasKey(t => t.Id);
-        modelBuilder.Entity<AftermarketCollectionModel>()
-            .ToTable("aftermarketcollection")
-            .HasKey(a => a.Id);
-        modelBuilder.Entity<PremiumBookingUpdate>()
-            .ToTable("premiumbookingupdate")
-            .HasKey(p => p.Id);
-        modelBuilder.Entity<SafetyStockItem>().HasNoKey();
+        public DbSet<ActivityForm> Activity { get; set; }
+        public DbSet<SplunkEntry> Splunk { get; set; }
+        public DbSet<HelperEntry> Helper { get; set; }
+        public DbSet<SpeedUpRequest> SpeedUpRequests { get; set; }
+        public DbSet<Premium2025> Premiums2025 { get; set; }
+        public DbSet<Premium2024> Premiums2024 { get; set; }
+        public DbSet<PartPrice> PartPrices { get; set; }
+        public DbSet<SafetyStockFormEntry> SafetyStockForms { get; set; }
+        public DbSet<TestCrudSafety> TestCrudSafeties { get; set; }
+        public DbSet<AftermarketCollectionModel> AftermarketCollections { get; set; }
+        public DbSet<SpeedUpModel> SpeedUps { get; set; }
+        public DbSet<PremiumBookingUpdate> PremiumBookingUpdates { get; set; }
+        public DbSet<ReportOut> ReportOuts { get; set; }
+
+        public AutomationDbContext(DbContextOptions<AutomationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ActivityForm>().ToTable("activity");
+            modelBuilder.Entity<SplunkEntry>()
+                .ToTable("splunk")
+                .HasKey(s => s.LoadNumber);
+            modelBuilder.Entity<HelperEntry>()
+                .ToTable("helper")
+                .HasKey(h => h.Id);
+            modelBuilder.Entity<SpeedUpRequest>()
+                .ToTable("ibl_emea_speedup_requests")
+                .HasKey(s => s.Id);
+            modelBuilder.Entity<Premium2025>()
+                .ToTable("premiums2025")
+                .HasKey(p => p.Id);
+            modelBuilder.Entity<Premium2024>()
+                .ToTable("premiums2024")
+                .HasKey(p => p.Id);
+            modelBuilder.Entity<PartPrice>()
+                .ToTable("Part_Price");
+            modelBuilder.Entity<SafetyStockFormEntry>()
+                .ToTable("safetystockform")
+                .HasKey(s => s.Id);
+            modelBuilder.Entity<TestCrudSafety>()
+                .ToTable("TestCrudSafety")
+                .HasKey(t => t.Id);
+            modelBuilder.Entity<AftermarketCollectionModel>()
+                .ToTable("aftermarketcollection")
+                .HasKey(a => a.Id);
+            modelBuilder.Entity<PremiumBookingUpdate>()
+                .ToTable("premiumbookingupdate")
+                .HasKey(p => p.Id);
+            modelBuilder.Entity<ReportOut>()
+                .ToTable("reportout")
+                .HasKey(r => r.Id);
+            modelBuilder.Entity<SafetyStockItem>().HasNoKey();
+        }
     }
 }
